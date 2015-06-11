a4d17421f57664dceedb2c5df167ec1fa0e77673
Thomas Elliott <tom.w.elliott@gmail.com>
2015-06-10 20:06:50 -0400

    Create new project with watchKit app and set up ignore for xcuserdata.

---
+++ SpeakerAlert/SpeakerAlert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeakerAlert
+//
+//  Created by Thomas Elliott on 6/10/15.
+//  Copyright © 2015 The Other Tom Elliott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SpeakerAlert/SpeakerAlertTests/SpeakerAlertTests.swift
@@ -0,0 +1,35 @@
+//
+//  SpeakerAlertTests.swift
+//  SpeakerAlertTests
+//
+//  Created by Thomas Elliott on 6/10/15.
+//  Copyright © 2015 The Other Tom Elliott. All rights reserved.
+//
+
+import XCTest
+
+class SpeakerAlertTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
