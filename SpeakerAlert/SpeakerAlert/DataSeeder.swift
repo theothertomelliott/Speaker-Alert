04b95f6fc24a93e2edb616ad9b9d51331e088752
Thomas Elliott <tom.w.elliott@gmail.com>
2015-08-23 19:06:46 -0400

    Add debug profile to the seed data (needs modifying so it only shows up in debug).

--- SpeakerAlert/SpeakerAlert/DataSeeder.swift
+++ SpeakerAlert/SpeakerAlert/DataSeeder.swift
@@ -108,6 +108,14 @@ class DataSeeder: NSObject {
             oneHour.red = 60*60
             oneHour.redBlink = 60*60 + 30
             
+            // TODO: Make this debug only
+            let debug : Profile = Profile.MR_createEntityInContext(localContext)
+            debug.name = "Debug (5 second intervals)"
+            debug.green = 5
+            debug.yellow = 10
+            debug.red = 15
+            debug.redBlink = 20
+
         }
     }
     
