8330af0ecccfee9e3e0f169f17538168b7374a56
Thomas Elliott <tom.w.elliott@gmail.com>
2015-08-23 23:23:46 -0400

    Hide the navigation and tab bar when a speech is in progress.

--- SpeakerAlert/SpeakerAlert/SpeechViewController.swift
+++ SpeakerAlert/SpeakerAlert/SpeechViewController.swift
@@ -12,16 +12,57 @@ class SpeechViewController: UIViewController, SpeechTimerDelegate {
     
     var speechMan : SpeechManager?;
     
+    // Code from: http://stackoverflow.com/questions/27008737/how-do-i-hide-show-tabbar-when-tapped-using-swift-in-ios8/27072876#27072876
+    func setTabBarVisible(visible:Bool, animated:Bool) {
+        
+        //* This cannot be called before viewDidLayoutSubviews(), because the frame is not set before this time
+        
+        // bail if the current state matches the desired state
+        if (tabBarIsVisible() == visible) { return }
+        
+        // get a frame calculation ready
+        let frame = self.tabBarController?.tabBar.frame
+        let height = frame?.size.height
+        let offsetY = (visible ? -height! : height)
+        
+        // zero duration means no animation
+        let duration:NSTimeInterval = (animated ? 0.3 : 0.0)
+        
+        //  animate the tabBar
+        if frame != nil {
+            UIView.animateWithDuration(duration) {
+                self.tabBarController?.tabBar.frame = CGRectOffset(frame!, 0, offsetY!)
+                return
+            }
+        }
+    }
+    
+    func tabBarIsVisible() ->Bool {
+        return self.tabBarController?.tabBar.frame.origin.y < CGRectGetMaxY(self.view.frame)
+    }
+    
+    // Call the function from tap gesture recognizer added to your view (or button)
+    
+    @IBAction func tapped(sender: AnyObject) {
+        setTabBarVisible(!tabBarIsVisible(), animated: true)
+    }
+    
     @IBAction func pausePressed(sender: AnyObject) {
         speechMan?.pause()
     }
     
     @IBAction func stopPressed(sender: AnyObject) {
         speechMan?.stop()
+        // TODO: show the status bar
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+        self.setTabBarVisible(true, animated: true)
     }
     
     @IBAction func resumePressed(sender: AnyObject) {
         speechMan?.start()
+        // TODO: hide the status bar
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+        self.setTabBarVisible(false, animated: true)
     }
     
     var blinkState : Bool = false
