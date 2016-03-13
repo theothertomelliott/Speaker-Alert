475ccc6ec88c72fc7e817cb8097f92da1132af0d
Thomas Elliott <tom.w.elliott@gmail.com>
2016-03-13 10:59:49 -0400

    Fix build issues and add missing assets for release via fastlane.
    Can now build and upload with: fastlane betafast

--- SpeakerAlert/SpeakerAlert-Bridging-Header.h
+++ SpeakerAlert/SpeakerAlert-Bridging-Header.h
@@ -11,7 +11,6 @@
 
 #import "Typhoon/Typhoon.h"
 #import <MagicalRecord/MagicalRecord.h>
-#import "TWETimeIntervalField/TWETimeIntervalField.h"
 #import "RFAboutView/RFAboutViewController.h"
 #import "Colours/Colours.h"
 
