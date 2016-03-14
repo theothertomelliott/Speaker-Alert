7fea178bcd659184fced887c8b9c71f2b4a35456
Thomas Elliott <tom.w.elliott@gmail.com>
2016-03-13 22:21:27 -0400

    Experimental UI test code for editing the profile name and updating/cancelling date selection.

--- SpeakerAlert/SpeakerAlertUITests/SpeakerAlertUITests.swift
+++ SpeakerAlert/SpeakerAlertUITests/SpeakerAlertUITests.swift
@@ -11,84 +11,90 @@ import XCTest
 
 class SpeakerAlertUITests: XCTestCase {
 
-    override func setUp() {
-        super.setUp()
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test.
-        // Doing this in setup will make sure it happens for each test method.
-        let app = XCUIApplication()
-        app.launchArguments = ["isUITesting"]
-        setupSnapshot(app)
-        app.launch()
-    }
+	override func setUp() {
+		super.setUp()
+		// In UI tests it is usually best to stop immediately when a failure occurs.
+		continueAfterFailure = false
+		// UI tests must launch the application that they test.
+		// Doing this in setup will make sure it happens for each test method.
+		let app = XCUIApplication()
+		app.launchArguments = ["isUITesting"]
+		setupSnapshot(app)
+		app.launch()
+	}
 
-    override func tearDown() {
-        super.tearDown()
-    }
+	override func tearDown() {
+		super.tearDown()
+	}
 
-    func testAddProfile() {
+	func testAddProfile() {
 
-        let app = XCUIApplication()
-        app.navigationBars["Profiles"].buttons["Add"].tap()
-        app.alerts["Add"].collectionViews.buttons["New Speech Profile"].tap()
+		let app = XCUIApplication()
+		app.navigationBars["Profiles"].buttons["Add"].tap()
+		app.alerts["Add"].collectionViews.buttons["New Speech Profile"].tap()
 
-        let tablesQuery = app.tables
-        let textField = tablesQuery.childrenMatchingType(.Cell).elementBoundByIndex(0).childrenMatchingType(.TextField).element
-        textField.tap()
+		let tablesQuery = app.tables
+		let textField = tablesQuery.childrenMatchingType(.Cell).elementBoundByIndex(0).childrenMatchingType(.TextField).element
+		textField.tap()
 
-        let deleteKey = app.keys["delete"]
-        deleteKey.tap()
-        deleteKey.tap()
-        deleteKey.tap()
-        deleteKey.tap()
-        deleteKey.tap()
-        deleteKey.tap()
-        deleteKey.tap()
-        deleteKey.tap()
-        deleteKey.tap()
-        deleteKey.tap()
-        deleteKey.tap()
-        app.buttons["shift"].tap()
-        textField.typeText("Test")
-        app.toolbars.buttons["Done"].tap()
-        tablesQuery.cells["Green"].staticTexts["0:00"].tap()
+//		let deleteKey = app.keys["delete"]
+//		deleteKey.tap()
+//		deleteKey.tap()
+//		deleteKey.tap()
+//		deleteKey.tap()
+//		deleteKey.tap()
+//		deleteKey.tap()
+//		deleteKey.tap()
+//		deleteKey.tap()
+//		deleteKey.tap()
+//		deleteKey.tap()
+//		deleteKey.tap()
+//		app.buttons["shift"]		.tap()
+//		textField.typeText("Test")
+//		app.toolbars.buttons["Done"].tap()
+		tablesQuery.cells["Green"]		.staticTexts["0:00"].tap()
 
-        app.pickerWheels.elementBoundByIndex(0).adjustToPickerWheelValue("1")
-        app.pickerWheels.elementBoundByIndex(2).adjustToPickerWheelValue("2")
-        app.pickerWheels.elementBoundByIndex(3).adjustToPickerWheelValue("3")
-        
-    }
+		// Wait until the button is there
+		let button = app.buttons["Cancel"]
+		let exists = NSPredicate(format: "exists == 1")
 
-    func testLoadProfileAndReturn() {
-        let app = XCUIApplication()
+		expectationForPredicate(exists, evaluatedWithObject: button, handler: nil)
+		waitForExpectationsWithTimeout(5, handler: nil)
 
-        // Wait until the defaults are loaded
-        let label = app.staticTexts["Five Minutes"]
-        let exists = NSPredicate(format: "exists == 1")
+//        app.pickerWheels.elementBoundByIndex(0).adjustToPickerWheelValue("1")
+//        app.pickerWheels.elementBoundByIndex(2).adjustToPickerWheelValue("2")
+//        app.pickerWheels.elementBoundByIndex(3).adjustToPickerWheelValue("3")
 
-        expectationForPredicate(exists, evaluatedWithObject: label, handler: nil)
-        waitForExpectationsWithTimeout(5, handler: nil)
+		app.buttons["Cancel"].tap()
+	}
 
-        // TODO: Base this off the seed data
-        XCTAssertEqual(app.tables.cells.count, 6)
+	func testLoadProfileAndReturn() {
+		let app = XCUIApplication()
 
-        app.tables.staticTexts["Five Minutes"].tap()
-        app.navigationBars["Five Minutes"].buttons["Profiles"].tap()
+		// Wait until the defaults are loaded
+		let label = app.staticTexts["Five Minutes"]
+		let exists = NSPredicate(format: "exists == 1")
 
-        XCTAssertEqual(app.tables.cells.count, 6)
+		expectationForPredicate(exists, evaluatedWithObject: label, handler: nil)
+		waitForExpectationsWithTimeout(5, handler: nil)
 
-        snapshot("Profile list")
-    }
+		// TODO: Base this off the seed data
+		XCTAssertEqual(app.tables.cells.count, 6)
 
-    func testLoadAboutScreen() {
-        let app = XCUIApplication()
-        let tabBarsQuery = app.tabBars
-        tabBarsQuery.buttons["Settings"].tap()
-        app.tables.staticTexts["About Speaker Alert"].tap()
-        app.navigationBars["About"].buttons["Settings"].tap()
-        tabBarsQuery.buttons["Speech"].tap()
+		app.tables.staticTexts["Five Minutes"].tap()
+		app.navigationBars["Five Minutes"].buttons["Profiles"].tap()
 
-    }
+		XCTAssertEqual(app.tables.cells.count, 6)
 
+		snapshot("Profile list")
+	}
+
+	func testLoadAboutScreen() {
+		let app = XCUIApplication()
+		let tabBarsQuery = app.tabBars
+		tabBarsQuery.buttons["Settings"].tap()
+		app.tables.staticTexts["About Speaker Alert"].tap()
+		app.navigationBars["About"].buttons["Settings"].tap()
+		tabBarsQuery.buttons["Speech"].tap()
+	}
 }
