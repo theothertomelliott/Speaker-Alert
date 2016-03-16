827ea10f15c3fdada922b0920fc03951b48e6e2b
Thomas Elliott <tom.w.elliott@gmail.com>
2016-03-16 08:39:29 -0400

    Keep track of the entered name so it is not lost when setting times. Remove the default name and use the hint as a guide. Do not allow saving empty names, and alert the user if they attempt to do so.

--- SpeakerAlert/SpeakerAlert/ProfileTableViewController.swift
+++ SpeakerAlert/SpeakerAlert/ProfileTableViewController.swift
@@ -22,7 +22,7 @@ class ProfileTableViewController: UITableViewController {
 				self.phaseTimes[SpeechPhase.RED] = NSTimeInterval(v.red!)
 				self.phaseTimes[SpeechPhase.OVER_MAXIMUM] = NSTimeInterval(v.redBlink!)
 			} else {
-				self.name = "New Profile"
+				self.name = ""
 				self.phaseTimes[SpeechPhase.GREEN] = 0
 				self.phaseTimes[SpeechPhase.YELLOW] = 0
 				self.phaseTimes[SpeechPhase.RED] = 0
@@ -90,32 +90,23 @@ class ProfileTableViewController: UITableViewController {
 			let cell = tableView.dequeueReusableCellWithIdentifier(identifier, forIndexPath: indexPath)
 
 			if let nameCell: ProfileNameTableViewCell = cell as? ProfileNameTableViewCell {
-
 				// Configure the cell...
 				if indexPath.row == 0 {
 					self.nameLabel = nameCell.profileName
-					if let profile = self.profile {
-						nameCell.profileName.text = profile.name
-					} else {
-						nameCell.profileName.text = "New Profile"
-					}
+					nameCell.profileName.text = self.name
 				}
 			}
 
 			if let timeCell: ProfileTimeTableViewCell = cell as? ProfileTimeTableViewCell {
-
 				if indexPath.row == 1 {
 					timeCell.setControllerAndPhase(self, phase: SpeechPhase.GREEN)
 				}
-
 				if indexPath.row == 2 {
 					timeCell.setControllerAndPhase(self, phase: SpeechPhase.YELLOW)
 				}
-
 				if indexPath.row == 3 {
 					timeCell.setControllerAndPhase(self, phase: SpeechPhase.RED)
 				}
-
 				if indexPath.row == 4 {
 					timeCell.setControllerAndPhase(self, phase: SpeechPhase.OVER_MAXIMUM)
 				}
@@ -124,40 +115,52 @@ class ProfileTableViewController: UITableViewController {
 			return cell
 	}
 
-    @IBAction func save() {
-        MagicalRecord.saveWithBlock({ (localContext: NSManagedObjectContext!) in
-            // This block runs in background thread
-            
-            if let p = self.profile {
-                let storedTiming: Profile = p.MR_inContext(localContext)
-                storedTiming.green = self.phaseTimes[SpeechPhase.GREEN]
-                storedTiming.yellow = self.phaseTimes[SpeechPhase.YELLOW]
-                storedTiming.red = self.phaseTimes[SpeechPhase.RED]
-                storedTiming.redBlink = self.phaseTimes[SpeechPhase.OVER_MAXIMUM]
-                storedTiming.name = self.nameLabel?.text
-            } else {
-                MagicalRecord.saveWithBlockAndWait({ (localContext: NSManagedObjectContext!) ->
-                    Void in
-                    // This block runs in background thread
-                    
-                    let newProfile: Profile = Profile.MR_createEntityInContext(localContext)
-                    newProfile.name = "New Speech Profile"
-                    newProfile.green = self.phaseTimes[SpeechPhase.GREEN]
-                    newProfile.yellow = self.phaseTimes[SpeechPhase.YELLOW]
-                    newProfile.red = self.phaseTimes[SpeechPhase.RED]
-                    newProfile.redBlink = self.phaseTimes[SpeechPhase.OVER_MAXIMUM]
-                    newProfile.name = self.nameLabel?.text
-                    if let pg: Group = self.parentGroup {
-                        newProfile.parent = pg.MR_inContext(localContext)
-                    }
-                })
-            }
-        })
-        self.dismiss()
-    }
-    
-    @IBAction func dismiss() {
-        self.navigationController?.popViewControllerAnimated(true)
-    }
-    
+	@IBAction func nameLabelFinishedEditing(sender: UITextView) {
+		self.name = sender.text
+	}
+
+	@IBAction func save() {
+		if self.name == "" {
+			let alertController = UIAlertController(title: "Name required", message:
+					"Please enter a name for your Speech Profile", preferredStyle: UIAlertControllerStyle.Alert)
+			alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+
+			self.presentViewController(alertController, animated: true, completion: nil)
+			return
+		}
+
+		MagicalRecord.saveWithBlock({ (localContext: NSManagedObjectContext!) in
+			// This block runs in background thread
+
+			if let p = self.profile {
+				let storedTiming: Profile = p.MR_inContext(localContext)
+				storedTiming.green = self.phaseTimes[SpeechPhase.GREEN]
+				storedTiming.yellow = self.phaseTimes[SpeechPhase.YELLOW]
+				storedTiming.red = self.phaseTimes[SpeechPhase.RED]
+				storedTiming.redBlink = self.phaseTimes[SpeechPhase.OVER_MAXIMUM]
+				storedTiming.name = self.nameLabel?.text
+			} else {
+				MagicalRecord.saveWithBlockAndWait({ (localContext: NSManagedObjectContext!) ->
+					Void in
+					// This block runs in background thread
+
+					let newProfile: Profile = Profile.MR_createEntityInContext(localContext)
+					newProfile.name = "New Speech Profile"
+					newProfile.green = self.phaseTimes[SpeechPhase.GREEN]
+					newProfile.yellow = self.phaseTimes[SpeechPhase.YELLOW]
+					newProfile.red = self.phaseTimes[SpeechPhase.RED]
+					newProfile.redBlink = self.phaseTimes[SpeechPhase.OVER_MAXIMUM]
+					newProfile.name = self.nameLabel?.text
+					if let pg: Group = self.parentGroup {
+						newProfile.parent = pg.MR_inContext(localContext)
+					}
+				})
+			}
+		})
+		self.dismiss()
+	}
+
+	@IBAction func dismiss() {
+		self.navigationController?.popViewControllerAnimated(true)
+	}
 }
