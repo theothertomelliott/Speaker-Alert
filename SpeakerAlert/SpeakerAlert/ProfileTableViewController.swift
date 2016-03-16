23beedd1345e82be6834cb4d71cb80158f60c92d
Thomas Elliott <tom.w.elliott@gmail.com>
2016-03-16 08:23:01 -0400

    Add OK/Cancel buttons to the speed edit view to avoid creating spurious new profiles when changing times.

--- SpeakerAlert/SpeakerAlert/ProfileTableViewController.swift
+++ SpeakerAlert/SpeakerAlert/ProfileTableViewController.swift
@@ -11,123 +11,123 @@ import MagicalRecord
 
 class ProfileTableViewController: UITableViewController {
 
-    var parentGroup: Group?
-    var profile: Profile? {
-        didSet {
-            if let v = self.profile {
-                // Populate internal values
-                self.name = v.name
-                self.phaseTimes[SpeechPhase.GREEN] = NSTimeInterval(v.green!)
-                self.phaseTimes[SpeechPhase.YELLOW] = NSTimeInterval(v.yellow!)
-                self.phaseTimes[SpeechPhase.RED] = NSTimeInterval(v.red!)
-                self.phaseTimes[SpeechPhase.OVER_MAXIMUM] = NSTimeInterval(v.redBlink!)
-            } else {
-                self.name = "New Profile"
-                self.phaseTimes[SpeechPhase.GREEN] = 0
-                self.phaseTimes[SpeechPhase.YELLOW] = 0
-                self.phaseTimes[SpeechPhase.RED] = 0
-                self.phaseTimes[SpeechPhase.OVER_MAXIMUM] = 0
-            }
-        }
-    }
+	var parentGroup: Group?
+	var profile: Profile? {
+		didSet {
+			if let v = self.profile {
+				// Populate internal values
+				self.name = v.name
+				self.phaseTimes[SpeechPhase.GREEN] = NSTimeInterval(v.green!)
+				self.phaseTimes[SpeechPhase.YELLOW] = NSTimeInterval(v.yellow!)
+				self.phaseTimes[SpeechPhase.RED] = NSTimeInterval(v.red!)
+				self.phaseTimes[SpeechPhase.OVER_MAXIMUM] = NSTimeInterval(v.redBlink!)
+			} else {
+				self.name = "New Profile"
+				self.phaseTimes[SpeechPhase.GREEN] = 0
+				self.phaseTimes[SpeechPhase.YELLOW] = 0
+				self.phaseTimes[SpeechPhase.RED] = 0
+				self.phaseTimes[SpeechPhase.OVER_MAXIMUM] = 0
+			}
+		}
+	}
 
-    private var nameLabel: UITextField?
+	private var nameLabel: UITextField?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
 
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
 
-        if let destination: ProfileTimeSelectorViewController =
-            segue.destinationViewController as? ProfileTimeSelectorViewController {
+		if let destination: ProfileTimeSelectorViewController =
+			segue.destinationViewController as? ProfileTimeSelectorViewController {
 
-            if let timeCell: ProfileTimeTableViewCell = sender as? ProfileTimeTableViewCell {
-                NSLog("\(timeCell.colorNameLabel.text)")
-                destination.colorName = timeCell.colorNameLabel.text
+				if let timeCell: ProfileTimeTableViewCell = sender as? ProfileTimeTableViewCell {
+					NSLog("\(timeCell.colorNameLabel.text)")
+					destination.colorName = timeCell.colorNameLabel.text
 
-                if let phase = timeCell.phase {
-                    destination.phase = phase
-                }
-                destination.profileUpdateReceiver = self
-                destination.profile = self.profile
-            }
-        }
-    }
+					if let phase = timeCell.phase {
+						destination.phase = phase
+					}
+					destination.profileUpdateReceiver = self
+					destination.profile = self.profile
+				}
+		}
+	}
 
-    // MARK: ProfileUpdateReceiver
+	// MARK: ProfileUpdateReceiver
 
-    var name: String?
-    var phaseTimes: [SpeechPhase : NSTimeInterval ] = [ : ]
+	var name: String?
+	var phaseTimes: [SpeechPhase: NSTimeInterval] = [:]
 
-    func phaseUpdated() {
-        self.tableView.reloadData()
-    }
+	func phaseUpdated() {
+		self.tableView.reloadData()
+	}
 
-    // MARK: - Table view data source
+	// MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
+	override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+		return 1
+	}
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-    }
+	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 6
+	}
 
+	override func tableView(
+		tableView: UITableView,
+		cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+			var identifier = indexPath.row == 0 ? "NameCell" : "TimeCell"
+			if indexPath.row == 5 {
+				identifier = "OkCancelCell"
+			}
 
-    override func tableView(
-        tableView: UITableView,
-        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let identifier = indexPath.row == 0 ? "NameCell" : "TimeCell"
+			let cell = tableView.dequeueReusableCellWithIdentifier(identifier, forIndexPath: indexPath)
 
-        let cell = tableView.dequeueReusableCellWithIdentifier(identifier, forIndexPath: indexPath)
+			if let nameCell: ProfileNameTableViewCell = cell as? ProfileNameTableViewCell {
 
-        if let nameCell: ProfileNameTableViewCell = cell as? ProfileNameTableViewCell {
+				// Configure the cell...
+				if indexPath.row == 0 {
+					self.nameLabel = nameCell.profileName
+					if let profile = self.profile {
+						nameCell.profileName.text = profile.name
+					} else {
+						nameCell.profileName.text = "New Profile"
+					}
+				}
+			}
 
-                // Configure the cell...
-                if indexPath.row == 0 {
-                    self.nameLabel = nameCell.profileName
-                    if let profile = self.profile {
-                        nameCell.profileName.text = profile.name
-                    } else {
-                        nameCell.profileName.text = "New Profile"
-                    }
-                }
+			if let timeCell: ProfileTimeTableViewCell = cell as? ProfileTimeTableViewCell {
 
-        }
+				if indexPath.row == 1 {
+					timeCell.setControllerAndPhase(self, phase: SpeechPhase.GREEN)
+				}
 
-        if let timeCell: ProfileTimeTableViewCell = cell as? ProfileTimeTableViewCell {
+				if indexPath.row == 2 {
+					timeCell.setControllerAndPhase(self, phase: SpeechPhase.YELLOW)
+				}
 
-                if indexPath.row == 1 {
-                    timeCell.setControllerAndPhase(self, phase: SpeechPhase.GREEN)
-                }
+				if indexPath.row == 3 {
+					timeCell.setControllerAndPhase(self, phase: SpeechPhase.RED)
+				}
 
-                if indexPath.row == 2 {
-                    timeCell.setControllerAndPhase(self, phase: SpeechPhase.YELLOW)
-                }
+				if indexPath.row == 4 {
+					timeCell.setControllerAndPhase(self, phase: SpeechPhase.OVER_MAXIMUM)
+				}
+			}
 
-                if indexPath.row == 3 {
-                    timeCell.setControllerAndPhase(self, phase: SpeechPhase.RED)
-                }
+			return cell
+	}
 
-                if indexPath.row == 4 {
-                    timeCell.setControllerAndPhase(self, phase: SpeechPhase.OVER_MAXIMUM)
-                }
-
-        }
-
-        return cell
-    }
-
-    override func viewWillDisappear(animated: Bool) {
+    @IBAction func save() {
         MagicalRecord.saveWithBlock({ (localContext: NSManagedObjectContext!) in
             // This block runs in background thread
-
+            
             if let p = self.profile {
                 let storedTiming: Profile = p.MR_inContext(localContext)
                 storedTiming.green = self.phaseTimes[SpeechPhase.GREEN]
@@ -136,10 +136,10 @@ class ProfileTableViewController: UITableViewController {
                 storedTiming.redBlink = self.phaseTimes[SpeechPhase.OVER_MAXIMUM]
                 storedTiming.name = self.nameLabel?.text
             } else {
-                MagicalRecord.saveWithBlockAndWait({(localContext: NSManagedObjectContext!) ->
+                MagicalRecord.saveWithBlockAndWait({ (localContext: NSManagedObjectContext!) ->
                     Void in
                     // This block runs in background thread
-
+                    
                     let newProfile: Profile = Profile.MR_createEntityInContext(localContext)
                     newProfile.name = "New Speech Profile"
                     newProfile.green = self.phaseTimes[SpeechPhase.GREEN]
@@ -153,8 +153,11 @@ class ProfileTableViewController: UITableViewController {
                 })
             }
         })
-
-
+        self.dismiss()
     }
-
+    
+    @IBAction func dismiss() {
+        self.navigationController?.popViewControllerAnimated(true)
+    }
+    
 }
