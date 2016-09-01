4e71f9172568a8feee4c9ec0cef0eaae0c4b4ca9
Thomas Elliott <tom.w.elliott@gmail.com>
2016-09-01 08:57:06 -0400

    Add check for correct time displayed in history.

--- SpeakerAlert/SpeakerAlertUITests/HistoryUITests.swift
+++ SpeakerAlert/SpeakerAlertUITests/HistoryUITests.swift
@@ -24,6 +24,7 @@ class HistoryUITests: SpeakerAlertUITests {
         app.tabBars.buttons["History"].tap()
         XCTAssertEqual(app.tables.cells.count, 1)
         XCTAssert(app.staticTexts["test speech"].exists)
+        XCTAssert(app.staticTexts["0:03"].exists)
     }
     
 }
